2cff1c15951162f748311d810d8f096fa21aaa88
Your Name <you@example.com>
2020-11-11 15:17:34 -0800

    work in progress

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,13 +46,21 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
-
+import React, { useState } from 'react'; /* STEP 0 */
 export default function Counter() {
   /* STEP 1 */
+  const [count, setCount] = useState(0);
+  const [isEven, setIsEven] = useState(true);
 
   const increment = () => {
     /* STEP 4 */
+    let newCount = count + 1;
+    setCount(newCount);
+    if (newCount % 2 !=0){
+      setIsEven(false);
+    } else{
+      setIsEven(true);
+    }
   };
   const decrement = () => {
     /* STEP 5 */
